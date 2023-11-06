7a3d873c6a5a8005deaaa87b72e2d08cd785fdbe
Chamster <nikkovkel258@gmail.com>
2023-11-06 15:39:37 +0300

    Trying to catch when browser close

--- 09/main.py
+++ 09/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
 import time
 
 class Browser:
@@ -9,7 +10,15 @@ class Browser:
 
     def open_by_url(self, url: str):
         self.driver.get(url)
-        time.sleep(5000)
+        self._wait_until_browser_is_closed()
+
+    def _wait_until_browser_is_closed(self):
+        try:
+            while True:
+                time.sleep(1)
+                self.driver.title
+        except WebDriverException:
+            print("Browser has been closed by user")
 
 if __name__ == "__main__":
     browser = Browser()
