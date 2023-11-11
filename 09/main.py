745c8e1bb4a915c53cacc8a2dee8756821b031c0
Chamster <nikkovkel258@gmail.com>
2023-11-11 21:26:09 +0300

    update

--- 09/main.py
+++ 09/main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 import time
-
+import os
 
 class Browser:
     def __init__(self):
@@ -15,15 +15,27 @@ class Browser:
         self.driver.get(url)
 
     def use_search(self):
-        div_with_input= self.driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div/div[3]/div/div[2]/div[2]/div/div[1]")
+        env_vars = os.environ
+        div_with_input = self.driver.find_element(
+            By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/div/div[3]/div/div[2]/div[2]/div/div[1]")
         div_with_input.click()
         time.sleep(1)
 
-        search = self.driver.find_element(By.XPATH, value="/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div/div[2]/div[3]/div/div/div/div/div[1]/div[1]/div[1]/input")
-        search.send_keys("BTC")
-        
+        search = self.driver.find_element(
+            By.XPATH, value="/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div/div[2]/div[3]/div/div/div/div/div[1]/div[1]/div[1]/input")
+        search.send_keys(env_vars["coin_name"])
+
         time.sleep(1)
         self._take_screenshot('screenshots/screenshot.png')
+
+        coin_elements = self.driver.find_elements(
+            By.CSS_SELECTOR, 'p.coin-name')
+
+        for coin in coin_elements:
+            if env_vars["coin_name"] in coin.text:
+                break
+        else:
+            raise Exception("Не пошло")
 
     def _take_screenshot(self, save_as: str):
         self.driver.save_screenshot(save_as)
@@ -42,3 +54,6 @@ if __name__ == "__main__":
     browser.open_by_url('https://coinmarketcap.com/')
     time.sleep(1)
     browser.use_search()
+
+    while True:
+        ...
